fa60b496c67216356879ac59b255e4c2f6f85027
Bibek Pant <pantbibek21@gmail.com>
2021-09-09 10:14:48 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+var radioChecker = () => {
+    var inputData = document.getElementById("data");
+    var radioBtn = document.querySelector("input[name='user-choice']:checked").value;
+
+    if (radioBtn == "number") {
+        inputData.setAttribute("placeholder", "Enter a number ...");
+        inputData.setAttribute("type", "number");
+    }
+    else {
+        inputData.setAttribute("placeholder", "Enter a string ...");
+        inputData.setAttribute("type", "text");
+    }
+}
+
+var palindromeChecker = () => {
+    var inputData = document.getElementById("data");
+    var attributeValue = inputData.getAttribute("type");
+    var result = document.querySelector(".result");
+
+    if (attributeValue == "number") {
+        var number = parseInt(inputData.value);
+        var reverseNumber = 0;
+        var tempNumber = number;
+        while (number != 0) {
+            var r = number % 10;
+            reverseNumber = reverseNumber * 10 + r;
+            number = Math.trunc(number / 10);
+        }
+
+        if (reverseNumber === tempNumber) {
+            result.style.opacity = 1;
+            result.innerText = "Yes, it's a palindrome !";
+        }
+        else {
+            result.style.opacity = 1;
+            result.innerText = "No, it's not a palindrome !";
+        }
+    }
+    else {
+        var string = inputData.value;
+        var stringArray = string.split("");
+        var reverseArray = stringArray.reverse();
+        var reversedString = reverseArray.join("");
+        if (reversedString == string) {
+            result.style.opacity = 1;
+            result.innerText = "Yes, it's a palindrome !";
+        }
+        else {
+            result.style.opacity = 1;
+            result.innerText = "No, it's not a palindrome !";
+        }
+    }
+}
+
+var resetData = () => {
+    var number = document.getElementById("data");
+    var result = document.querySelector(".result");
+    number.value = "";
+    result.style.opacity = 0;
+}
